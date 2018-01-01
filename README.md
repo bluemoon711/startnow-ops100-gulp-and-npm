e3a8d2ef6a9029939f84b8abbf1d3d063a7c99a3
Ying Fan <yingcathyfan@gmail.com>
2018-01-01 14:13:19 -0800

    adds gulp project files

---
+++ README.md
@@ -0,0 +1,6 @@
+In this project, I create a local git repository and a gulp build process, then 
+push a branch with lastest code up to Github.
+
+a gulp script: gulpfile.js
+
+
---
+++ server/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(express.static('public'));
+app.use(express.static('dist'));
+
+module.exports = app;
+
+{ 
+    "scripts" : {
+        "test" : "mocha test"
+        "build" : "gulp build:js && gulp build:css"
+    }
+}
